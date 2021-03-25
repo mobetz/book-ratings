9d24aeeb6974a526b723a27479fd69858b97534d
Matthew Obetz <obetzm@rpi.edu>
2021-03-24 20:17:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+import fs from 'fs';
+import csv from 'csv-parser'
+
+
+
+let results = {}
+
+function readCSV(stream_loc, functor) {
+    return new Promise((resolve, reject) => {
+        let stream = fs.createReadStream(stream_loc)
+            .setEncoding("utf-8")
+            .pipe(csv({separator: ";"}))
+
+        stream.on("data", functor);
+        stream.on("end", () => resolve(results));
+        stream.on("error", error => reject(error));
+    });
+}
+
+
+
+export default readCSV('./BX-Books.csv', (data) => {
+    results[data.ISBN] = data;
+    results[data.ISBN].ratings = []
+}).then((book_data) => {
+        return readCSV('./BX-Book-Ratings.csv', (data) => {
+            if ( book_data[data.ISBN] && data.BookRating > 0) {
+                book_data[data.ISBN].ratings.push(parseInt(data.BookRating));
+            }
+        })
+    })
+    .then((rated_books) => {
+        return Object.values(rated_books)
+            .filter((book) => book.ratings.length > 0)
+            .map((book) => {
+                book.NumRatings = book.ratings.length;
+                book.AverageScore = book.ratings.reduce((x,y) => x+y) / book.ratings.length;
+                return book;
+            })
+    });
+
+
